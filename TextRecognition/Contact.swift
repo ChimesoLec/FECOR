3eeab99d0c37f655fb7f08bb0917dc9ada2c04d4
Luis <lec@chimeso.net>
2017-05-29 13:37:33 -0400

    Added sqlite module, created contacs DB, started a parsers, and added rudamentary front end

---
+++ TextRecognition/Contact.swift
@@ -0,0 +1,9 @@
+//
+//  Contact.swift
+//  TextRecognition
+//
+//  Created by Luis Collado on 5/27/17.
+//  Copyright © 2017 Luis Collado. All rights reserved.
+//
+
+import Foundation
---
+++ TextRecognition/SecondVeiw.swift
@@ -0,0 +1,9 @@
+//
+//  SecondVeiw.swift
+//  TextRecognition
+//
+//  Created by Luis Collado on 5/27/17.
+//  Copyright © 2017 Luis Collado. All rights reserved.
+//
+
+import Foundation
